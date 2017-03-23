53e936ac057db1d4234224fad5be2c5bbbdb19a1
Arthur Abelyan <art23.dev@gmail.com>
2017-03-23 17:55:46 +0300

    form fix

--- src/index.js
+++ src/index.js
@@ -232,15 +232,29 @@ var clusterer = new ymaps.Clusterer({
         getPointAddress(coords)
             .then(
                 function(response) {
-                  myMap.balloon.open(coords, {
-                      contentHeader: response.GeoObject.description + ' ' + response.GeoObject.name,
-                      contentBody:'<p>Кто-то щелкнул по карте.</p>' +
-                          '<p>Координаты щелчка: ' + [
-                          coords[0].toPrecision(6),
-                          coords[1].toPrecision(6)
-                          ].join(', ') + '</p>',
-                      contentFooter:'<sup><a data-add="true">Добавить метку</a></sup>'
-                  });
+                  myMap.balloon.open(coords,
+                    `<div class="popup">
+                    <div class="popup__header">
+                        <span class="popup__header-inner">
+                            <i class="fa fa-map-marker popup__header-marker" aria-hidden="true"></i>
+                            <span class="popup__header-text">${response.GeoObject.description} ${response.GeoObject.name}</span>
+                        </span>
+                    </div>
+                    <div class="popup__main">
+                        <div class="popup__main-reviews" id="reviews">
+                            asd
+                        </div>
+                        <div class="popup__main-add clearfix">
+                            <div class="popup__main-add-header">ВАШ ОТЗЫВ</div>
+                            <form name="form" method="POST">
+                                <input type="text" name="name" placeholder="Ваше имя" class="popup__input">
+                                <input type="text" name="place" placeholder="Укажите место" class="popup__input">
+                                <textarea name="message" placeholder="Поделитесь впечатлениями" class="popup__textarea"></textarea>
+                                <button class="btn" id="btn">Добавить</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>`, );
                 }
             )
             .catch()
